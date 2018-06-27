41386a875ae4589d0ed0fb79232f1df81cd567db
Pete Peterson <petersonpf@ornl.gov>
2018-06-27 08:10:25 -0400

    Attempting to fix script

--- update_mantid_git.sh
+++ update_mantid_git.sh
@@ -3,6 +3,9 @@
 # update the checked out repositories
 set -x
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 # create the _drafts directory if it isn't already present
 if [ ! -d _drafts ]; then
     mkdir _drafts
@@ -27,10 +30,11 @@ fi
 # update the ticket list
 $PYTHON_EXE tools/get_pull_requests.py --repo mantidproject/mantid || exit 1
 
+# commit the news page
+git add _drafts/week*.md
+
 # stop now if there are no changes
-if [ $(git diff --quiet _drafts/week*.md) ]; then
-    # commit the news page
-    git add _drafts/week*.md
+if [ ! $(git diff --quiet origin/master _drafts/week*.md) ]; then
     git commit -m "Updating ticket list via jenkins"
 
     # push the changes to master
